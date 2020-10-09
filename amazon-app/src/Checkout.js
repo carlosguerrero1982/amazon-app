daa4b83102692ed094798d0225aabdb10684c037
= <=>
2020-10-09 12:54:59 +0200

    091020:1254

--- amazon-app/src/Checkout.js
+++ amazon-app/src/Checkout.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {useStateValue} from './StateProvider';
 import './Checkout.css';
 import CheckoutProduct from './CheckoutProduct';
-
+import Subtotal from './Subtotal';
+import CurrencyFormat from "react-currency-format";
 
 function Checkout() {
 
@@ -14,7 +15,12 @@ function Checkout() {
     return (
         <div className="checkout">
             
-            <img className="checkout_ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+        <div className="checkout_left">
+
+            <img className="checkout_ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""
+            
+            
+            />
     
             {basket?.length===0 ? (
 
@@ -40,16 +46,28 @@ function Checkout() {
                         price={item.price}
                         rating={item.rating}
                         />
-                    ))
-                }
-                
+                    ))}
                 
                 </div>
-            )
+               )}
+         </div>
+
+         {basket.length > 0 &&(
         
-        }
+        <div className="checkout_right">
+        
+        <Subtotal />
+
         </div>
-    )
+    
+       )}
+    
+   </div>
+       
+    );
+    
+
+    
 }
 
 export default Checkout
